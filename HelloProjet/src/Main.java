f025fd77b02e6c0040c5072c5db6159ecb4b7642
ChineseBXT <754042308@qq.com>
2019-05-29 17:54:45 +0800

    最后一次提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,4 +1,4 @@
-﻿import org.junit.Test;
+import org.junit.Test;
 
 public class Main {
 
@@ -6,6 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("不行Hello!");
-
+		System.out.println("最后一次Hello!");
     }
 }
